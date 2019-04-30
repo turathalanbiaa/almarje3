dcac4e05cd06c5b580e8538991ca551174744bab
Ahmed <ahmedfelah@gmail.com>
2019-04-30 14:49:50 +0300

    Parents Category

--- resources/views/components/parentsCategory.blade.php
+++ resources/views/components/parentsCategory.blade.php
@@ -3,7 +3,7 @@
 
 
 <div class="mb-5">
-    <a  href="{{route('index')}}" class="btn btn-link p-1">الصفحة الرذيسية</a>
+    <a  href="{{route('index')}}" class="btn btn-link p-1">الصفحة الرئيسية</a>
     @foreach($categories as $category)
         <span>-</span>
         <a class="p-1" href="{{route('viewCategories', $category['id'])}}" class="btn btn-link">{{$category['title']}}</a>
